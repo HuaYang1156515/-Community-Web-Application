2d164e52f30fb444476b6aefdb55399e3d4e9348
hua yang <360892334@qq.com>
2024-08-03 15:42:15 +1200

    change

--- run.py
+++ run.py
@@ -4,3 +4,11 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+#create database
+from app import db, create_app
+from app.models import User
+
+app = create_app()
+app.app_context().push()
+db.create_all()
