70ff719b42289c12363cebcd413d058985128c1b
hua yang <360892334@qq.com>
2024-08-08 15:47:47 +1200

    change

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
-from . import db
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,13 +9,13 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'User {self.username}'
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<Event {self.title}>'
+        return f'Event {self.title}'
