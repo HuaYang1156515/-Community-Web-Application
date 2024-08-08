fc688dbef781424baf6bba0eb009ba3046a317e4
hua yang <360892334@qq.com>
2024-08-08 12:23:19 +1200

    change

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,10 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     db.init_app(app)
 
-    from .main import main as main_blueprint
+    from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
