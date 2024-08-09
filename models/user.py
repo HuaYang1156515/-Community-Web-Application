ce719664e539adf547500268536d130e613979f9
hua yang <360892334@qq.com>
2024-08-09 16:07:51 +1200

    hashing

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 from app import db
 from flask_login import UserMixin
 
+# user_loader function
 class User(UserMixin, db.Model):
     __tablename__ = 'web_user'
     id = db.Column(db.Integer, primary_key=True)
@@ -12,15 +13,3 @@ class User(UserMixin, db.Model):
     pic = db.Column(db.String(255), nullable=True)
     desc = db.Column(db.String(255), nullable=True)
 
-"""
-class User(db.Model):
-    __tablename__ = 'web_user'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    login = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    login_time = db.Column(db.DateTime, nullable=True)
-    status = db.Column(db.String(2), default='1', nullable=False)
-    pic = db.Column(db.String(255), nullable=True)
-    desc = db.Column(db.String(255), nullable=True)
-"""
