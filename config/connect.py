72130f64e27cab682157e50b6040bcb77fbe4064
hua yang <360892334@qq.com>
2024-09-05 11:42:27 +1200

    Update connect.py

--- config/connect.py
+++ config/connect.py
@@ -1,12 +1,18 @@
 import mysql.connector
 from mysql.connector import Error
 
+"""
 # Database connection details
 dbuser = "root"   
 dbpass = "abcd1234"   
 dbhost = "localhost" 
 dbport = "3306"
 dbname = "community_event_planner"
+"""
 
 
-
+dbuser = 'HuaYang'  # Your PythonAnywhere username
+dbpass = 'your_password'  # Your MySQL database password
+dbhost = 'HuaYang.mysql.pythonanywhere-services.com'  # Your MySQL host
+dbport = '3306'  # Default MySQL port
+dbname = 'HuaYang$HuaYang777'  # Replace with your chosen database name
