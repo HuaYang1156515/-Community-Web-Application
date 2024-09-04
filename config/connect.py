217c0db0a3f8edd9ba4584ac187d264e63bf252a
hua yang <360892334@qq.com>
2024-09-05 09:56:45 +1200

    change

--- config/connect.py
+++ config/connect.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from mysql.connector import Error
-"""
+
 # Database connection details
 dbuser = "root"   
 dbpass = "abcd1234"   
@@ -8,46 +8,19 @@ dbhost = "localhost"
 dbport = "3306"
 dbname = "community_event_planner"
 
-def get_connection():
-    
-    try:
-        connection = mysql.connector.connect(
-            host=dbhost,
-            user=dbuser,
-            password=dbpass,
-            database=dbname,
-            port=dbport
-        )
-        if connection.is_connected():
-            print("Connected to the database")
-            return connection
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
-        return None
-"""
 
 
-
- 
 db_config = {
-    'user': 'HuaYang',  # Your MySQL username
-    'password': 'abcd1234',  # Your MySQL password
-    'host': 'HuaYang.mysql.pythonanywhere-services.com',  # Hostname for MySQL
-    'database': 'HuaYang$HuaYang777',  # Your new database name
-    'port': 3306  # MySQL port, should be an integer, not a string
+    'user': 'HuaYang',                   # MySQL username
+    'password': 'abcd1234',              # MySQL password
+    'host': 'HuaYang.mysql.pythonanywhere-services.com', # MySQL host
+    'database': 'community_event_planner',    # MySQL database name
+    'port': 3306                         # MySQL port, if needed
 }
 
-def get_connection():
-    try:
-        connection = mysql.connector.connect(**db_config)
-        if connection.is_connected():
-            print("Connected to the database")
-            return connection
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
-        return None
-
-# Test the connection
-conn = get_connection()
-if conn:
-    conn.close()  # Remember to close the connection when done
+try:
+    connection = mysql.connector.connect(**db_config)
+    if connection.is_connected():
+        print("Connected to the database")
+except mysql.connector.Error as e:
+    print(f"Error connecting to MySQL: {e}")
