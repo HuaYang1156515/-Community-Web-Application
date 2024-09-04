90f4f6c21d4adb4bad3682fb4df14252a417ffd6
hua yang <360892334@qq.com>
2024-09-05 10:52:38 +1200

    Update connect.py

--- config/connect.py
+++ config/connect.py
@@ -10,17 +10,3 @@ dbname = "community_event_planner"
 
 
 
-db_config = {
-    'user': 'HuaYang',                   # MySQL username
-    'password': 'abcd1234',              # MySQL password
-    'host': 'HuaYang.mysql.pythonanywhere-services.com', # MySQL host
-    'database': 'community_event_planner',    # MySQL database name
-    'port': 3306                         # MySQL port, if needed
-}
-
-try:
-    connection = mysql.connector.connect(**db_config)
-    if connection.is_connected():
-        print("Connected to the database")
-except mysql.connector.Error as e:
-    print(f"Error connecting to MySQL: {e}")
